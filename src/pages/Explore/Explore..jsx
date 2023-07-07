071c1d235f77eccdb0110fcb8c53b3a8c0e9e341
jayajput30 <rajputjay901@gmail.com>
2023-07-07 18:10:34 +0530

    feat: added navbar for mobile/desktop to all pages

--- src/pages/Explore/Explore..jsx
+++ src/pages/Explore/Explore..jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import styles from "./Explore.module.css";
+import { useLocation } from "react-router-dom";
+import { checkPageLocation } from "../../utils/utls,";
+import MobileNavbar from "../../components/Navbar/MobileNavbar/MobileNavbar";
+import DesktopNavbar from "../../components/Navbar/DesktopNavbar/DesktopNavbar";
 const Explore = () => {
-  return <div>Explore.</div>;
+  const location = useLocation();
+  const checkPath = checkPageLocation(location.pathname);
+
+  return (
+    <div className={styles.exploreWrapper}>
+      {!checkPath && <MobileNavbar />}
+      {!checkPath && <DesktopNavbar />}
+    </div>
+  );
 };
 
 export default Explore;
